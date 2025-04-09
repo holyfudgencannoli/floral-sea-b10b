3b3826e4f6916a9bcb60e5db7d7bd9a83b8774fe
holyfudgencannoli <moshimoshi8246@gmail.com>
2025-04-09 09:01:36 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,12 @@
-import { renderHtml } from "./renderHtml";
-
 export default {
   async fetch(request, env) {
+    // Run a SQL query using the D1 binding
     const stmt = env.DB.prepare("SELECT * FROM comments LIMIT 3");
     const { results } = await stmt.all();
 
-    return new Response(renderHtml(JSON.stringify(results, null, 2)), {
-      headers: {
-        "content-type": "text/html",
-      },
+    // Return the results as a JSON response
+    return new Response(JSON.stringify(results), {
+      headers: { "Content-Type": "application/json" },
     });
   },
 } satisfies ExportedHandler<Env>;
